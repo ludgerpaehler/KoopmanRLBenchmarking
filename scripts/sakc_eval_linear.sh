50ec8131a2757a5bd1a36d25275803470cfff497
Ludger Paehler <ludger.paehler@tum.de>
2024-02-18 14:47:10 -0800

    Script for the evaluation.

---
+++ scripts/sakc_eval_linear.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in LinearSystem-v0
+do for j in 83 103 123 143 163
+  do
+      python -m evaluations.value_based_sac_continuous_action_eval \
+        --env-id=$i \
+        --exp-name=koopman_${j}_eval \
+        --total-timesteps=20000 \
+        --state-dict=/Users/lpaehler/Work/ReinforcementLearning/KoopmanRL/DataLogs/Interpretability_Table/saved_models/${i}/${j}/value_based_sakc_actor.pt \
+        --koopman
+  done
+done
